8b436d215144c493b81f8676f2a76972bb38c0d8
Yash Soni <39760283+yashsoni27@users.noreply.github.com>
2025-03-14 00:13:07 +0000

    Initial countdown card and SVG filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,37 +7,21 @@ export default async function Home() {
   
 
   return (
-    <main className="flex p-24">
-      <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+    <main className="p-20 gap-5">
+      <div className="flex flex-row gap-5">
         <CountdownCard />
-        <Link
+        {/* <CountdownCard /> */}
+        {/* <Link
           href="/dashboard"
           className="p-5 border rounded-lg hover:bg-gray-900 transition-colors"
         >
           <h2 className="text-2xl">Dashboard</h2>
           <p>View current F1 season overview</p>
-        </Link>
-        <Link
-          href="/drivers"
-          className="p-5 border rounded-lg hover:bg-gray-900 transition-colors"
-        >
-          <h2 className="text-2xl">Drivers</h2>
-          <p>View driver standings and statistics</p>
-        </Link>
-        <Link
-          href="/constructors"
-          className="p-5 border rounded-lg hover:bg-gray-900 transition-colors"
-        >
-          <h2 className="text-2xl">Constructors</h2>
-          <p>View constructor standings and statistics</p>
-        </Link>
-        <Link
-          href="/races"
-          className="p-5 border rounded-lg hover:bg-gray-900 transition-colors"
-        >
-          <h2 className="text-2xl">Races</h2>
-          <p>View race calendar and results</p>
-        </Link>
+        </Link>*/}
+      </div>
+      <div className="pt-5">
+        {/* <CountdownCard /> */}
+        
       </div>
     </main>
   );
--- src/components/ui/CountdownCard.tsx
+++ src/components/ui/CountdownCard.tsx
@@ -1,5 +1,6 @@
-"use client"
+"use client";
 import { getNextRace } from "@/lib/api";
+import Image from "next/image";
 import React, { useState, useEffect } from "react";
 
 interface CountdownProps {
@@ -7,10 +8,41 @@ interface CountdownProps {
   raceName: string;
 }
 
+function DynamicSVG({ name }: {name: string}) {
+  const [SVGComponent, setSVGComponent] = useState(null);
+
+  useEffect(() => {
+    async function loadSVG() {
+      try {
+        const { default: LoadedSVG } = await import(
+          `../../components/circuits/${name}.svg`
+        );
+        setSVGComponent(() => LoadedSVG); // Store the component function
+      } catch (error) {
+        // console.error("Failed to load SVG:", error);
+      }
+    }
+
+    loadSVG();
+  }, [name]);
+
+  if (!SVGComponent) {
+    return null;
+  }
+
+  return (
+    <>
+      <Image className="mr-5 h-20 w-24 svg-filter" src={SVGComponent} alt={name + "circuit"} />
+    </>
+  );
+  // return <SVGComponent fill="#fff" />;
+}
+
 const CountdownCard = () => {
   const [season, setSeason] = useState("");
   const [raceName, setRaceName] = useState("");
   const [raceDate, setRaceDate] = useState("");
+  const [circuitId, setCircuitId] = useState("");
   const [timeLeft, setTimeLeft] = useState({
     days: 0,
     hours: 0,
@@ -22,10 +54,11 @@ const CountdownCard = () => {
     const fetchNextRace = async () => {
       try {
         const nextRace = await getNextRace();
-        // console.log(nextRace);
+        console.log(nextRace);
         if (nextRace) {
           setSeason(nextRace.season);
           setRaceName(nextRace.raceName);
+          setCircuitId(nextRace.Circuit.circuitId);
           let timeStamp = new Date(`${nextRace.date}T${nextRace.time}`);
 
           setRaceDate(timeStamp.toISOString());
@@ -63,30 +96,35 @@ const CountdownCard = () => {
     return () => clearInterval(timer);
   }, [raceDate]);
 
-
   return (
-    <div className="bg-gradient-to-tr from-red-900 to-red-700 p-5 rounded-lg shadow-lg border border-red-600 w-96 text-center">
-      <h2 className="text-2xl font-semibold text-left">{season} {raceName}</h2>
-      <div className="flex justify-start space-x-4 text-xl mt-2">
-        <div>
-          <p className="font-semibold">{timeLeft.days}</p>
-          <p className="text-sm font-thin">DAYS</p>
+    <div className="bg-gradient-to-tr from-red-900 to-red-700 p-5 rounded-lg shadow-lg border border-red-600 w-[400px] text-center">
+      <h2 className="text-2xl font-semibold text-left">
+        {season} {raceName}
+      </h2>
+
+      <div className="flex justify-between items-center">
+        <div className="flex justify-start align-middle space-x-4 text-xl">
+          <div className="">
+            <p className="font-semibold">{timeLeft.days}</p>
+            <p className="text-sm font-thin">DAYS</p>
+          </div>
+          <div>
+            <p className="font-semibold">{timeLeft.hours}</p>
+            <p className="text-sm font-thin">HRS</p>
+          </div>
+          <div>
+            <p className="font-semibold">{timeLeft.minutes}</p>
+            <p className="text-sm font-thin">MINS</p>
+          </div>
+          <div>
+            <p className="font-semibold">{timeLeft.seconds}</p>
+            <p className="text-sm font-thin">SEC</p>
+          </div>
         </div>
-        <div>
-          <p className="font-semibold">{timeLeft.hours}</p>
-          <p className="text-sm font-thin">HRS</p>
-        </div>
-        <div>
-          <p className="font-semibold">{timeLeft.minutes}</p>
-          <p className="text-sm font-thin">MINS</p>
-        </div>
-        <div>
-          <p className="font-semibold">{timeLeft.seconds}</p>
-          <p className="text-sm font-thin">SEC</p>
-        </div>
+        <DynamicSVG name={circuitId} />
       </div>
     </div>
   );
-}
+};
 
 export default CountdownCard;
