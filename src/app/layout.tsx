f186bc7a5f25912ec4ced1698b5904472bad2585
Yash Soni <39760283+yashsoni27@users.noreply.github.com>
2025-04-30 12:14:31 +0100

    refactor: update layout component for improved readability and change project title to 'Delta Dash'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,19 +4,22 @@ import { Titillium_Web } from "next/font/google";
 import localFont from "next/font/local";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
-import { Analytics } from '@vercel/analytics/next';
-
+import { Analytics } from "@vercel/analytics/next";
 
 // const myFont = Titillium_Web({ weight: ["200", "300", "400", "600", "700", "900"], subsets: ["latin"] });
 const myFont = localFont({
-  src: '../../public/fonts/Formula1.otf', // Path relative to public folder
+  src: "../../public/fonts/Formula1.otf", // Path relative to public folder
   weight: "100",
   style: "normal",
 });
 
 export const metadata: Metadata = {
-  title: "F1 Dashboard",
+  title: "Delta Dash",
   description: "Formula 1 analysis dashboard",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
@@ -28,7 +31,12 @@ export default function RootLayout({
     <html lang="en">
       <body className={myFont.className}>
         <Navbar />
-        <main id="main-content" className="min-h-screen transition-all duration-300 ml-60">{children}</main>
+        <main
+          id="main-content"
+          className="min-h-screen transition-all duration-300 ml-60"
+        >
+          {children}
+        </main>
         <Footer />
         <Analytics />
       </body>
